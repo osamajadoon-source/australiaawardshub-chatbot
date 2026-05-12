36c00bac8342c6686b14e52f39e19d84923136d0
osamajadoon-source <osamajadoon@gmail.com>
2026-05-12 22:20:54 +1000

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+        ],
+      },
+    ];
+  },
+};
+module.exports = nextConfig;
